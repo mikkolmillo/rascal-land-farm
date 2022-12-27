de3eb6782b060adb5f537c5e3744f5bf878236bf
Donmikko Dan M. Olmillo <57443799+mikkolmillo@users.noreply.github.com>
2022-12-27 13:50:37 +0800

    fix router view

--- app/gem-farm/src/App.vue
+++ app/gem-farm/src/App.vue
@@ -6,26 +6,25 @@
       <img :width="50" :height="50" src="./assets/400x600.gif" />
       <p class="text-4xl pt-3 mt-1 px-2 text-black underline">LAND</p>
     </div>
-    <!-- <div class="italic mt-5 text-center">by Gemworks</div> -->
+    <div class="italic mt-5 text-center">by Gamedust</div>
 
     <div class="p-10">
-      <!-- <router-view /> -->
-      <Farmer />
+      <router-view />
     </div>
 
     <div class="pt-10"></div>
     <!-- <TheCat /> -->
     <!-- <TheFooter /> -->
+    <Gooey />
   </div>
 </template>
 
 <script>
-import Farmer from '@/views/Farmer.vue';
-// import TheNavBar from '@/components/TheNavBar';
-// import TheFooter from '@/components/gem-farm/TheFooter';
-// import TheCat from '@/components/gem-farm/TheCat';
+import TheNavBar from '@/components/TheNavBar';
+import TheFooter from '@/components/gem-farm/TheFooter';
+import TheCat from '@/components/gem-farm/TheCat';
 export default {
-  components: { Farmer },
+  components: {},
 };
 </script>
 

02ed39548b9a6f3b5d6a23ad9018031a38196823
Donmikko Dan M. Olmillo <57443799+mikkolmillo@users.noreply.github.com>
2022-12-28 10:24:30 +0800

    update heading section

--- app/gem-farm/src/App.vue
+++ app/gem-farm/src/App.vue
@@ -1,10 +1,28 @@
 <template>
   <div class="relative h-full min-h-screen">
     <!-- <TheNavBar /> -->
-    <div class="pt-10 px-10 flex justify-center align-middle">
-      <p class="text-4xl pt-3 mt-1 text-black underline">RASCAL LAND</p>
+    <div class="bg-white">
+      <div class="mx-auto max-w-7xl pt-10 px-4 sm:px-6 lg:px-8">
+        <div class="text-center">
+          <p
+            class="
+              mt-1
+              text-4xl
+              font-bold
+              tracking-tight
+              text-gray-900
+              sm:text-5xl
+              lg:text-6xl
+            "
+          >
+            Rascal Land
+          </p>
+          <h2 class="text-lg pt-1 font-semibold text-indigo-600 italic">
+            by Gamedust
+          </h2>
+        </div>
+      </div>
     </div>
-    <div class="italic mt-5 text-center">by Gamedust</div>
 
     <div class="p-10">
       <router-view />
